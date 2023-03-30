a38dd2886e1f9fb47f92a67f68e18f0799fab6c6
sajjad <samaralishaikh212@gmail.com>
2023-03-30 10:51:45 +0530

    initial commit 🎭

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl text-stone-950 font-bold">hello</h1>
+    </main>
+  );
+}
