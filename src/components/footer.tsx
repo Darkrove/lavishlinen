de3d9ac741450121265704c8a0ddc431e57efca4
sajjad <samaralishaikh212@gmail.com>
2023-04-05 18:03:29 +0530

    update: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,25 +5,28 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer class="relative z-10 border-t border-stone-900 bg-white">
-      <div class="container max-w-7xl mx-auto p-5 md:p-10">
-        <div class="-mx-4 flex flex-col md:flex-row justify-between">
-          <div class="w-full px-4 sm:w-2/3 lg:w-3/12">
-            <div class="mb-10 w-full">
-              <Link href="/" className="mr-6 flex items-center space-x-2 mb-4">
-                <Icons.logo className="h-6 w-6" />
-                <span className="font-bold">{siteConfig.name}</span>
+    <footer className="relative z-10 border-t border-stone-900 bg-white">
+      <div className="container max-w-7xl mx-auto p-5 md:p-10">
+        <div className="-mx-4 flex flex-col md:flex-row justify-between">
+          <div className="w-full px-4 sm:w-2/3 lg:w-3/12">
+            <div className="mb-10 w-full">
+              <Link
+                href="/"
+                classNameName="mr-6 flex items-center space-x-2 mb-4"
+              >
+                <Icons.logo classNameName="h-6 w-6" />
+                <span classNameName="font-bold">{siteConfig.name}</span>
               </Link>
-              <p class="text-body-color mb-7 text-base">
+              <p className="text-body-color mb-7 text-base">
                 Discover the Joy of Luxurious Living
               </p>
-              <p class="text-dark flex items-center text-sm font-medium">
-                <span class="text-primary mr-3">
+              <p className="text-dark flex items-center text-sm font-medium">
+                <span className="text-primary mr-3">
                   <svg
                     width="19"
                     height="21"
                     viewBox="0 0 19 21"
-                    class="fill-current"
+                    className="fill-current"
                   >
                     <path d="M17.8076 11.8129C17.741 11.0475 17.1088 10.5151 16.3434 10.5151H2.16795C1.40261 10.5151 0.803643 11.0808 0.703816 11.8129L0.00502514 18.8008C-0.0282506 19.2001 0.104853 19.6327 0.371059 19.9322C0.637265 20.2317 1.03657 20.398 1.46916 20.398H17.0755C17.4748 20.398 17.8741 20.2317 18.1736 19.9322C18.4398 19.6327 18.5729 19.2334 18.5396 18.8008L17.8076 11.8129ZM17.2751 19.1668C17.2419 19.2001 17.1753 19.2667 17.0422 19.2667H1.46916C1.36933 19.2667 1.2695 19.2001 1.23623 19.1668C1.20295 19.1336 1.1364 19.067 1.16968 18.9339L1.86847 11.9127C1.86847 11.7463 2.00157 11.6465 2.16795 11.6465H16.3767C16.5431 11.6465 16.6429 11.7463 16.6762 11.9127L17.375 18.9339C17.3417 19.0337 17.3084 19.1336 17.2751 19.1668Z" />
                     <path d="M9.25704 13.1106C7.95928 13.1106 6.92773 14.1422 6.92773 15.4399C6.92773 16.7377 7.95928 17.7693 9.25704 17.7693C10.5548 17.7693 11.5863 16.7377 11.5863 15.4399C11.5863 14.1422 10.5548 13.1106 9.25704 13.1106ZM9.25704 16.6046C8.6248 16.6046 8.09239 16.0722 8.09239 15.4399C8.09239 14.8077 8.6248 14.2753 9.25704 14.2753C9.88928 14.2753 10.4217 14.8077 10.4217 15.4399C10.4217 16.0722 9.88928 16.6046 9.25704 16.6046Z" />
@@ -34,14 +37,14 @@ const Footer = () => {
               </p>
             </div>
           </div>
-          <div class="w-full px-4 sm:w-1/2 lg:w-2/12">
-            <div class="mb-10 w-full">
-              <h4 class="text-dark mb-3 text-lg font-semibold">Shop All</h4>
+          <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
+            <div className="mb-10 w-full">
+              <h4 className="text-dark mb-3 text-lg font-semibold">Shop All</h4>
               <ul>
                 <li>
                   <a
                     href="javascript:void(0)"
-                    class="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                    className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
                   >
                     Full sleeve shirts
                   </a>
@@ -49,7 +52,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="javascript:void(0)"
-                    class="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                    className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
                   >
                     Half sleeve shirts
                   </a>
@@ -57,7 +60,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="javascript:void(0)"
-                    class="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                    className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
                   >
                     Pure linen fabric
                   </a>
@@ -66,14 +69,16 @@ const Footer = () => {
             </div>
           </div>
 
-          <div class="w-full px-4 sm:w-1/2 lg:w-2/12">
-            <div class="mb-10 w-full">
-              <h4 class="text-dark mb-3 text-lg font-semibold">Quick Links</h4>
+          <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
+            <div className="mb-10 w-full">
+              <h4 className="text-dark mb-3 text-lg font-semibold">
+                Quick Links
+              </h4>
               <ul>
                 <li>
                   <a
                     href="javascript:void(0)"
-                    class="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                    className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
                   >
                     Track order
                   </a>
@@ -81,7 +86,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="javascript:void(0)"
-                    class="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                    className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
                   >
                     Contact
                   </a>
@@ -89,7 +94,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="javascript:void(0)"
-                    class="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                    className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
                   >
                     Blog
                   </a>
@@ -97,7 +102,7 @@ const Footer = () => {
                 <li>
                   <a
                     href="javascript:void(0)"
-                    class="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
+                    className="text-body-color hover:text-primary mb-2 inline-block text-base leading-loose"
                   >
                     About
                   </a>
@@ -105,64 +110,66 @@ const Footer = () => {
               </ul>
             </div>
           </div>
-          <div class="w-full px-4 sm:w-1/2 lg:w-3/12">
-            <div class="mb-10 w-full">
-              <h4 class="text-dark mb-3 text-lg font-semibold">Follow Us On</h4>
-              <div class="mb-6 flex items-center">
+          <div className="w-full px-4 sm:w-1/2 lg:w-3/12">
+            <div className="mb-10 w-full">
+              <h4 className="text-dark mb-3 text-lg font-semibold">
+                Follow Us On
+              </h4>
+              <div className="mb-6 flex items-center">
                 <a
                   href="javascript:void(0)"
-                  class="text-dark hover:bg-primary hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-stone-500 sm:mr-4 lg:mr-3 xl:mr-4"
+                  className="text-dark hover:bg-primary hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-stone-500 sm:mr-4 lg:mr-3 xl:mr-4"
                 >
                   <svg
                     width="8"
                     height="16"
                     viewBox="0 0 8 16"
-                    class="fill-current"
+                    className="fill-current"
                   >
                     <path d="M7.43902 6.4H6.19918H5.75639V5.88387V4.28387V3.76774H6.19918H7.12906C7.3726 3.76774 7.57186 3.56129 7.57186 3.25161V0.516129C7.57186 0.232258 7.39474 0 7.12906 0H5.51285C3.76379 0 2.54609 1.44516 2.54609 3.5871V5.83226V6.34839H2.10329H0.597778C0.287819 6.34839 0 6.63226 0 7.04516V8.90323C0 9.26452 0.243539 9.6 0.597778 9.6H2.05902H2.50181V10.1161V15.3032C2.50181 15.6645 2.74535 16 3.09959 16H5.18075C5.31359 16 5.42429 15.9226 5.51285 15.8194C5.60141 15.7161 5.66783 15.5355 5.66783 15.3806V10.1419V9.62581H6.13276H7.12906C7.41688 9.62581 7.63828 9.41935 7.68256 9.10968V9.08387V9.05806L7.99252 7.27742C8.01466 7.09677 7.99252 6.89032 7.85968 6.68387C7.8154 6.55484 7.61614 6.42581 7.43902 6.4Z" />
                   </svg>
                 </a>
                 <a
                   href="javascript:void(0)"
-                  class="text-dark hover:bg-primary hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-stone-500 sm:mr-4 lg:mr-3 xl:mr-4"
+                  className="text-dark hover:bg-primary hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-stone-500 sm:mr-4 lg:mr-3 xl:mr-4"
                 >
                   <svg
                     width="16"
                     height="12"
                     viewBox="0 0 16 12"
-                    class="fill-current"
+                    className="fill-current"
                   >
                     <path d="M14.2194 2.06654L15.2 0.939335C15.4839 0.634051 15.5613 0.399217 15.5871 0.2818C14.8129 0.704501 14.0903 0.845401 13.6258 0.845401H13.4452L13.3419 0.751468C12.7226 0.258317 11.9484 0 11.1226 0C9.31613 0 7.89677 1.36204 7.89677 2.93542C7.89677 3.02935 7.89677 3.17025 7.92258 3.26419L8 3.73386L7.45806 3.71037C4.15484 3.61644 1.44516 1.03327 1.00645 0.587084C0.283871 1.76125 0.696774 2.88845 1.13548 3.59296L2.0129 4.90802L0.619355 4.20352C0.645161 5.18982 1.05806 5.96477 1.85806 6.52838L2.55484 6.99804L1.85806 7.25636C2.29677 8.45401 3.27742 8.94716 4 9.13503L4.95484 9.36986L4.05161 9.93346C2.60645 10.8728 0.8 10.8024 0 10.7319C1.62581 11.7652 3.56129 12 4.90323 12C5.90968 12 6.65806 11.9061 6.83871 11.8356C14.0645 10.2857 14.4 4.41487 14.4 3.2407V3.07632L14.5548 2.98239C15.4323 2.23092 15.7935 1.8317 16 1.59687C15.9226 1.62035 15.8194 1.66732 15.7161 1.6908L14.2194 2.06654Z" />
                   </svg>
                 </a>
                 <a
                   href="javascript:void(0)"
-                  class="text-dark hover:bg-primary hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-stone-500 sm:mr-4 lg:mr-3 xl:mr-4"
+                  className="text-dark hover:bg-primary hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-stone-500 sm:mr-4 lg:mr-3 xl:mr-4"
                 >
                   <svg
                     width="16"
                     height="12"
                     viewBox="0 0 16 12"
-                    class="fill-current"
+                    className="fill-current"
                   >
                     <path d="M15.6645 1.88018C15.4839 1.13364 14.9419 0.552995 14.2452 0.359447C13.0065 6.59222e-08 8 0 8 0C8 0 2.99355 6.59222e-08 1.75484 0.359447C1.05806 0.552995 0.516129 1.13364 0.335484 1.88018C0 3.23502 0 6 0 6C0 6 0 8.79263 0.335484 10.1198C0.516129 10.8664 1.05806 11.447 1.75484 11.6406C2.99355 12 8 12 8 12C8 12 13.0065 12 14.2452 11.6406C14.9419 11.447 15.4839 10.8664 15.6645 10.1198C16 8.79263 16 6 16 6C16 6 16 3.23502 15.6645 1.88018ZM6.4 8.57143V3.42857L10.5548 6L6.4 8.57143Z" />
                   </svg>
                 </a>
                 <a
                   href="javascript:void(0)"
-                  class="text-dark hover:bg-primary hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-stone-500 sm:mr-4 lg:mr-3 xl:mr-4"
+                  className="text-dark hover:bg-primary hover:border-primary mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-[#E5E5E5] hover:text-stone-500 sm:mr-4 lg:mr-3 xl:mr-4"
                 >
                   <svg
                     width="14"
                     height="14"
                     viewBox="0 0 14 14"
-                    class="fill-current"
+                    className="fill-current"
                   >
                     <path d="M13.0214 0H1.02084C0.453707 0 0 0.451613 0 1.01613V12.9839C0 13.5258 0.453707 14 1.02084 14H12.976C13.5432 14 13.9969 13.5484 13.9969 12.9839V0.993548C14.0422 0.451613 13.5885 0 13.0214 0ZM4.15142 11.9H2.08705V5.23871H4.15142V11.9ZM3.10789 4.3129C2.42733 4.3129 1.90557 3.77097 1.90557 3.11613C1.90557 2.46129 2.45002 1.91935 3.10789 1.91935C3.76577 1.91935 4.31022 2.46129 4.31022 3.11613C4.31022 3.77097 3.81114 4.3129 3.10789 4.3129ZM11.9779 11.9H9.9135V8.67097C9.9135 7.90323 9.89082 6.8871 8.82461 6.8871C7.73571 6.8871 7.57691 7.74516 7.57691 8.60323V11.9H5.51254V5.23871H7.53154V6.16452H7.55423C7.84914 5.62258 8.50701 5.08065 9.52785 5.08065C11.6376 5.08065 12.0232 6.43548 12.0232 8.2871V11.9H11.9779Z" />
                   </svg>
                 </a>
               </div>
-              <p class="text-body-color text-base">
+              <p className="text-body-color text-base">
                 &copy; 2023 {siteConfig.name}
               </p>
             </div>
