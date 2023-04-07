0a9af04b7686213ef72193956d92d91e6adab1c5
sajjad <samaralishaikh212@gmail.com>
2023-04-07 16:59:16 +0530

    update: product view page error

--- src/app/(products)/products/[permalink]/page.tsx
+++ src/app/(products)/products/[permalink]/page.tsx
@@ -24,7 +24,7 @@ const ProductPage = async ({
   });
 
   return (
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div>
         <Image
           src={product.assets[0].url}
