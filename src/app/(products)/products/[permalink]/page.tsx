d1b9ac0544011b1a3f7d4902624ebb580427558d
sajjad <samaralishaikh212@gmail.com>
2023-04-07 16:53:38 +0530

    update: product view page error

--- src/app/(products)/products/[permalink]/page.tsx
+++ src/app/(products)/products/[permalink]/page.tsx
@@ -23,9 +23,6 @@ const ProductPage = async ({
     type: "permalink",
   });
 
-  const handleImageClick = (index: number) => {
-    pass;
-  };
   return (
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div>
