37d5f6a0e60d717fc02cebb2254dea4fc06925b5
sajjad <samaralishaikh212@gmail.com>
2023-04-25 16:29:23 +0530

    update: select value for better UX

--- src/components/category-sorter.tsx
+++ src/components/category-sorter.tsx
@@ -12,8 +12,27 @@ interface CategorySorterProps {
   onSortChange: (sortType: string) => void;
 }
 
+interface SortOptions {
+  [key: string]: string;
+  featured: string;
+  "best-selling": string;
+  "a-z": string;
+  "z-a": string;
+  "low-high": string;
+  "high-low": string;
+}
+
 const CategorySorter = ({ onSortChange }: CategorySorterProps): JSX.Element => {
   const [sortType, setSortType] = useState("");
+  const mapping: SortOptions = {
+    featured: "Featured",
+    "best-selling": "Best Selling",
+    "a-z": "A-Z",
+    "z-a": "Z-A",
+    "low-high": "Low to High",
+    "high-low": "High to Low",
+  };
+
   useEffect(() => {
     onSortChange(sortType);
   }, [sortType, onSortChange]);
@@ -22,7 +41,7 @@ const CategorySorter = ({ onSortChange }: CategorySorterProps): JSX.Element => {
     <Select value={sortType} onValueChange={setSortType}>
       <SelectTrigger className="w-[180px]">
         <SelectValue placeholder="Sort By" aria-label={sortType}>
-          {sortType ? sortType : "Sort By"}
+          {sortType ? mapping[sortType] : "Sort By"}
         </SelectValue>
       </SelectTrigger>
       <SelectContent>
