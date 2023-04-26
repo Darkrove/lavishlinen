f6b1da07203b4b89ec800cae835fd734a9effde2
sajjad <samaralishaikh212@gmail.com>
2023-04-26 20:05:02 +0530

    update: footer component

---
+++ src/config/footer.ts
@@ -0,0 +1,62 @@
+export const footerConfig = {
+  QuickLinks: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Contact",
+      href: "/contact",
+    },
+    {
+      title: "Track Order",
+      href: "/",
+    },
+  ],
+  ShopByCategory: [
+    {
+      title: "Full Sleeve",
+      href: "/category/full-sleeve-shirts",
+    },
+    {
+      title: "Half Sleeve",
+      href: "/category/half-sleeve-shirts",
+    },
+    {
+      title: "Linen Fabrics",
+      href: "/category/pure-linen-fabrics",
+    },
+  ],
+  Social: [
+    {
+      title: "Instagram",
+      href: "https://www.instagram.com/lavish_linen_/",
+      external: true,
+      icon: "instagram",
+    },
+    {
+      title: "Faecbook",
+      href: "https://www.facebook.com/people/lavish_linen_/100089931697810/",
+      external: true,
+      icon: "facebook",
+    },
+  ],
+  Policy: [
+    {
+      title: "Refund Policy",
+      href: "/refund-policy",
+    },
+    {
+      title: "Shipping Policy",
+      href: "/shipping-policy",
+    },
+    {
+      title: "Return Policy",
+      href: "/return-policy",
+    },
+    {
+      title: "COD Policy",
+      href: "/cod-policy",
+    },
+  ],
+};
