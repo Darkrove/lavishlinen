6a71e2ca3bf75a70ac2016446b3ac44467d9cfeb
sajjad <samaralishaikh212@gmail.com>
2023-04-27 19:18:43 +0530

    update: logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -36,25 +36,14 @@ import {
   Twitter,
   User,
   X,
+  Command,
   type Icon as LucideIcon,
 } from "lucide-react";
 
 export type Icon = LucideIcon;
 
 export const Icons = {
-  logo: (props: LucideProps) => (
-    <svg
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <circle cx="12" cy="12" r="10"></circle>
-    </svg>
-  ),
+  logo: Command,
   close: X,
   spinner: Loader2,
   chevronLeft: ChevronLeft,
